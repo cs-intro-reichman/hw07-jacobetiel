d53c139953c33385990750a1457591848ec83771
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-12-28 17:58:05 +0200

    test3

--- Palindrome.java
+++ Palindrome.java
@@ -10,7 +10,6 @@ public class Palindrome {
 		int length = s.length();
 		if (length == 1 || length <= 0) return true;
 		if(s.charAt(0)!=s.charAt(length-1)) return false;
-		isPalindrome(s.substring(1,length-1));
-		return true;
+		return isPalindrome(s.substring(1,length-1));
     }
 }
